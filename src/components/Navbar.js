aac76bb939dfea0ebb9e56d25898fca77a66bd7b
saintjohn1992 <saintjohnshane@outlook.com>
2022-07-26 20:59:31 +0200

    dividing the navbar, installed mui dependencies, and styling the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,48 @@
+import { Search } from '@mui/icons-material'
 import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
   height: 60px;
-  background-color: black;
-
   `
+ const Wrapper = styled.div`
+ padding: 10px 20px;
+ display: flex;
+ justify-content: space-between
+ ` 
+ const Left = styled.div`
+ flex: 1;
+
+ `
+ const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+ `
+ const SearchContainer = styled.div`
+ border: 1px solid lightgray
+ `
+
+ const Center = styled.div`
+ flex: 1;
+ `
+ const Right = styled.div`
+ flex: 1;
+ `
 
 const Navbar = () => {
   return (
     <Container>
-      Navbar
+     <Wrapper>
+        <Left>
+            <Language>EN</Language>
+            <SearchContainer>
+                input
+                <Search />
+            </SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+     </Wrapper>
     </Container>
   )
 }
